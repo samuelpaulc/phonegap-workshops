569a2311c20e10b0f6a3b50fc793c938bf48585d
Samuel Chandrasegaran <samuel.chandrasegaran@oracle.com>
2013-12-26 18:39:23 +0530

    test fragment created via js

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -14,11 +14,32 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
+        });        
         //this.store = new LocalStorageStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    
+    renderHomeView: function() {
+        var html =
+                "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     }
-
 };
 
 app.initialize();
