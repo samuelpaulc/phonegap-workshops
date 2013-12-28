09632228bc61fcb3135c57fda3a82a8512101394
Samuel Chandrasegaran <samuel.chandrasegaran@oracle.com>
2013-12-28 22:24:34 +0530

    upto part 6 from tutorial

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,37 +1,9 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
-    initialize: function() {
-        this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
-            self.renderHomeView();
-        });        
-        //this.store = new LocalStorageStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-    
-    renderHomeView: function() {
-        var html =
-                "<div class='header'><h1>Home</h1></div>" +
-                "<div class='search-view'>" +
-                "<input class='search-key'/>" +
-                "<ul class='employee-list'></ul>" +
-                "</div>"
-        $('body').html(html);
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
+    var self = this;
+    this.store = new MemoryStore(function() {
+        $('body').html(new HomeView(self.store).render().el);
+    });
     
     showAlert: function (message, title) {
         if (navigator.notification) {
